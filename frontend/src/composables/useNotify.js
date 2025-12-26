c247854f2600d9524e2f33d7fd83412a4da651f5
Marcos Vinicius <marcos.10823460@gmail.com>
2025-12-26 19:09:15 -0300

    First commit

---
+++ frontend/src/composables/useNotify.js
@@ -0,0 +1,15 @@
+import { Notify } from "quasar";
+
+export function useNotify(){
+    const notify = (message,type,position,timeout) => {
+    Notify.create({
+        message: message,
+        type: type,
+        position: position,
+        timeout: timeout
+    })
+}
+return{
+    notify
+}
+}
---
+++ frontend/src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { Quasar, Notify } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(Quasar, {
+    plugins:{
+        Notify
+    }
+})
+
+app.mount('#app')
