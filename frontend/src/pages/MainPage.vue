f321af599cfb6aeb62e0a045c21a4284154b64eb
Marcos Vinicius <marcos.10823460@gmail.com>
2025-12-27 18:13:37 -0300

    feat:configuracao-tabela-de-exebicao-de-lancamentos

--- frontend/src/pages/MainPage.vue
+++ frontend/src/pages/MainPage.vue
@@ -1,6 +1,37 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import { ListarLancamentosService } from '@/service/CarregarService';
 import GerarLancamento from '@/components/GerarLancamento.vue';
+
+//Armazena os lançamentos carregados da API
+const lancamentos = ref([])
+
+//Consulta os lançamentos na API
+async function carregarLancamentos() {
+    try {
+        const dados = await ListarLancamentosService.listar()
+        console.log('Dados da api', dados)
+
+        // Verifica se os dados são um array antes de atribuir
+        lancamentos.value = Array.isArray(dados) ? dados : []
+        console.log('Lançamentos carregados:', lancamentos.value)
+    } catch (error) {
+        console.error('Erro ao carregar lançamentos:', error)
+        lancamentos.value = []
+    }
+}
+//Carrega os lançamentos ao montar o componente
+onMounted(() => {
+    carregarLancamentos()
+})
+
+//Define as colunas da tabela
+const colunas = [
+    { name: 'data', label: 'Data', field: 'datavenc', align: 'left' },
+    { name: 'descricao', label: 'Descrição', field: 'descricao', align: 'left' },
+    { name: 'valor', label: 'Valor', field: 'valor', align: 'right' },
+    { name: 'tipo', label: 'Tipo', field: 'tipo' },
+]
 
 // Controle de exibição do modal
 const mostrarModal = ref(false);
@@ -21,9 +52,21 @@ function salvarLancamento(dados) {
     fecharModal()
 }
 
+
+
 </script>
 
 <template>
+
+    <!--Tabela de lançamentos-->
+    <q-table
+        title="Lançamentos Recentes"
+        :rows="lancamentos"
+        :columns="colunas"
+        row-key="id"
+        flat
+        bordered
+    />
     <!--Botão para abrir o modal-->
     <q-btn color="primary" label="Primary" @click="abrirModal" />
 
@@ -34,6 +77,4 @@ function salvarLancamento(dados) {
     </q-dialog>
 </template>
 
-<style scoped>
-    
-</style>
+<style scoped></style>
