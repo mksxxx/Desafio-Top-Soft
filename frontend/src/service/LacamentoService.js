457a3feeec6c47ddefc42403b330da2bffb7b119
Marcos Vinicius <marcos.10823460@gmail.com>
2025-12-26 22:00:42 -0300

    Correção

--- frontend/src/service/LacamentoService.js
+++ frontend/src/service/LacamentoService.js
@@ -1,17 +1,24 @@
+//URL da API
 const API_URL = 'http://localhost:3000';
 export const LancamentoService = {
     async criar(dados) {
         try {
+            //Salvar Lançamento no banco
             const resposta = await fetch(`${API_URL}/cadastro`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(dados)
-            }); if (!resposta.ok) throw new Error('Erro ao salvar no servidor');
+            }); 
+            //Verifica se a resposta foi ok
+            if (resposta.ok) throw new Error('Erro ao salvar no servidor');
 
             return await resposta.json();
-        } catch (error) {
+
+        } 
+        //Tratamento de erro
+        catch (error) {
             console.error("Erro no serviço:", error);
             throw error;
             }
