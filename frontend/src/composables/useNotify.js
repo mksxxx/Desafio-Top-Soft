2cb2efc278a993711023608f5d7f70edff9dbaa3
Marcos Vinicius <marcos.10823460@gmail.com>
2025-12-26 22:03:46 -0300

    feat/notify-global

--- frontend/src/composables/useNotify.js
+++ frontend/src/composables/useNotify.js
@@ -1,16 +1,16 @@
 import { Notify } from "quasar";
 
-export function useNotify(){
-    const notify = (message,type,position,timeout) => {
-    Notify.create({
-        message: message,
-        type: type,
-        position: position,
-        timeout: timeou
-        aa
-    })
-}
-return{
-    notify
-}
+export function useNotify() {
+    const notify = (message, type, position, timeout) => {
+        Notify.create({
+            message: message,
+            type: type,
+            position: position,
+            timeout: timeout
+
+        })
+    }
+    return {
+        notify
+    }
 }
